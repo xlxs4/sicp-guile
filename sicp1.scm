bc08f6869cd9db23eee43fedee8affd985d47827
xlxs4 <orousoultzoglou@gmail.com>
2023-02-12 01:37:08 +0200

    exercise: Add 1.12

--- sicp1.scm
+++ sicp1.scm
@@ -528,3 +528,24 @@
                  (- count 1)))))
   (iter 2 1 0 (- n 2)))
 
+;;; Exercise 1.12
+;; The following pattern of numbers is called Pascal's triangle.
+
+;;           1
+;;         1   1
+;;       1   2   1
+;;    1    3    3   1
+;; 1    4    6    4    1
+;;         . . .
+
+;; The numbers at the edge of the triangle are all 1, and each number
+;; inside the triangle is the sum of the two numbers above it.
+;; Write a procedure that computes elements of Pascal's triangle
+;; by means of a recursive process.
+
+(define (pascal row col)
+  (if (or (= row col) (= col 1))
+      1
+      (+ (pascal (- row 1) (- col 1))
+         (pascal (- row 1) col))))
+
