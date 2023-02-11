c95e4ddc70aa537d39a21ba159505cc49eb64b8b
xlxs4 <orousoultzoglou@gmail.com>
2023-02-11 20:27:53 +0200

    exercise: Add exercise 1.9

--- sicp1.scm
+++ sicp1.scm
@@ -1,6 +1,9 @@
 ;;; Utilities
 (define (square x) (* x x))
-(define (average a b) (/ (+ a b) 2))
+(define (average x y) (/ (+ x y) 2))
+
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
 
 ;;; Exercise 1.1
 ;; Below is a sequence of expressions. What is the result printed by
@@ -364,7 +367,9 @@
 
 (define (my-cube x)
   (define (improve guess)
-    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+    (/ (+ (/ x (square guess))
+          (* 2 guess))
+       3))
   (define (good-enough? guess)
     (= guess (improve guess)))
   (define (iter guess)
@@ -372,4 +377,54 @@
         guess
         (iter (improve guess))))
   (iter 1.0))
+
+;;; Exercise 1.9
+;; Each of the following two procedures defines a method
+;; for adding two positive integers in terms of the procedures
+;; inc, which increments its argument by 1, and dec,
+;; which decrements its argument by 1.
+
+;; (define (+ a b)
+;;   (if (= a 0)
+;;       b
+;;       (inc (+ (dec a) b))))
+
+;; (define (+ a b)
+;;   (if (= a 0)
+;;       b
+;;       (+ (dec a) (inc b))))
+
+;; Using the substitution model, illustrate the process generated
+;; by each procedure in evaluating (+ 4 5). Are these processes
+;; iterative or recursive?
+
+;;; Answer:
+;; Let's take a look at the first version <trace simplified>:
+
+;; scheme@(guile-user)> ,trace (+ 4 5)
+;; trace: |  (+ 4 5)
+;; trace: |  |  (+ 3 5)
+;; trace: |  |  |  (+ 2 5)
+;; trace: |  |  |  |  (+ 1 5)
+;; trace: |  |  |  |  |  (+ 0 5)
+;; trace: |  |  |  |  |  5
+;; trace: |  |  |  |  6
+;; trace: |  |  |  7
+;; trace: |  |  8
+;; trace: |  9
+
+;; And the second version <trace simplified>:
+
+;; scheme@(guile-user)> ,trace (+ 4 5)
+;; trace: |  (+ 4 5)
+;; trace: |  (+ 3 6)
+;; trace: |  (+ 2 7)
+;; trace: |  (+ 1 8)
+;; trace: |  (+ 0 9)
+;; trace: |  9
+
+;; Both versions are a definition of a recursive procedure.
+;; However, the first version generates a recursive process,
+;; while the second version generates an iterative process.
+;; Take a look at 1.2.1 "Linear Recursion and Iteration".
 
