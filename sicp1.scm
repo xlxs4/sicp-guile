05aa482febac90ec02242c0fed8e734a014ff841
xlxs4 <orousoultzoglou@gmail.com>
2023-02-13 02:29:26 +0200

    exercise: Add 1.16

--- sicp1.scm
+++ sicp1.scm
@@ -754,3 +754,41 @@
 
 ;; The order of growth of space and of number of steps is Θ(log α).
 
+;;; Exercise 1.16
+;; Design a procedure that evolves an iterative exponentation process
+;; that uses successive squaring and uses a logarithmic number of steps,
+;; as does fast-expt. (Hint: Using the observation that
+;; (b^(n/2))^2 = (b^2)^(n/2), keep, along with the exponent n
+;; and the base b, an additional state variable a,
+;; and define the transformation in such a way that
+;; the product ab^n is unchanged from state to state. At the beginning
+;; of the process a is taken to be 1, and the answer is given by the value
+;; of a at the end of the process. In general, the technique of defining
+;; an invariant quantity that remains unchainged from state to state
+;; is a powerful way to think about the design of iterative algorithms.)
+
+;;; Answer:
+;; We want to have ab^n unchanged from state to state.
+;; We need to think about two cases:
+;; When n is even, ab^n can be rewritten as:
+;; ab^n = a(b^(n/2)) = a(b^2)^(n/2)
+;; This can be written as a'(b')^n',
+;; where a' = a, b' = b^2, n' = n/2
+
+;; Similarly, when n is odd, ab^n can be rewritten as:
+;; ab^n = abb^(n - 1) = a'(b')^n'
+;; where a' = ab, b' = b, n' = n - 1
+;; That's all we need.
+
+(define (fast-expt b n)
+  (define (iter b n a)
+    (cond ((= n 0)
+           a)
+          ((even? n)
+           (iter (square b) (/ n 2) a))
+          (else
+           (iter b (- n 1) (* a b)))))
+  (iter b n 1))
+
+;; This is O(1) for space and O(log n) for number of steps.
+
