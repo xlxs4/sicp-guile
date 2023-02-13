a25e7fec8b278a6b7fa2ded6ea9c7b7cd93390c7
xlxs4 <orousoultzoglou@gmail.com>
2023-02-13 18:19:08 +0200

    exercise: Add 1.21

--- sicp1.scm
+++ sicp1.scm
@@ -965,3 +965,19 @@
 ;; 4 during the final reduction evaluation phase.
 ;; Using applicative-order evaluation, remainder is called 4 times.
 
+;;; Exercise 1.21
+;; Use the smallest-divisor procedure to find the smallest divisor
+;; of each of the following numbers: 199, 1999, 19999.
+
+;;; Answer:
+;; scheme@(guile-user)> (smallest-divisor 199)
+;; 199
+
+;; scheme@(guile-user)> (smallest-divisor 1999)
+;; 1999
+
+;; scheme@(guile-user)> (smallest-divisor 19999)
+;; 7
+
+;; We can see that both 199 and 1999 are primes, while 19999 isn't.
+
