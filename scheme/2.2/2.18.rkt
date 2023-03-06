f202e611afe6b7064ffc0d0420eaab7bcad74802
xlxs4 <orousoultzoglou@gmail.com>
2023-03-06 19:08:37 +0200

    exercise: Add 2.18

---
+++ scheme/2.2/2.18.rkt
@@ -0,0 +1,14 @@
+;;; Exercise 2.18
+;; Define a procedure reverse that takes a list as
+;; argument and returns a list of the same elements
+;; in reverse order:
+
+;; (reverse (list 1 4 9 16 25))
+;; (25 16 9 4 1)
+
+(define (reverse xs)
+  (define (iter l1 l2)
+    (if (null? l1)
+        l2
+        (iter (cdr l1) (cons (car l1) l2))))
+  (iter xs '()))
