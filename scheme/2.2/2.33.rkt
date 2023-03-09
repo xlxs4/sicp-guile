5fd1592d4d52120a05065c16e015bb7c87cbeb21
xlxs4 <orousoultzoglou@gmail.com>
2023-03-09 22:27:38 +0200

    exercise: Add 2.33

---
+++ scheme/2.2/2.33.rkt
@@ -0,0 +1,33 @@
+;;; Exercise 2.33
+;; Fill in the missing epressions to complete
+;; the following definitions of some basic
+;; list-manipulation operations as accumulations:
+
+;; (define (map p sequence)
+;;   (accumulate (lambda (x y) <??>)
+;;               nil sequence))
+
+;; (define (append seq1 seq2)
+;;   (accumulate cons <??> <??>))
+
+;; (define (length sequence)
+;;   (accumulate <??> 0 sequence))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y))
+              '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1))
+              0 sequence))
